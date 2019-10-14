7a9a3261c216dfc2bd0ddf9a6da0a47d44bf9eb3
0xAshish <a4ashishrp@gmail.com>
2019-10-14 19:22:14 +0530

    update truffle and solidity compiler, add openzeppelin

--- contracts/ValidatorSet.sol
+++ contracts/ValidatorSet.sol
@@ -1,4 +1,4 @@
-pragma solidity 0.5.9;
+pragma solidity ^0.5.11;
 
 interface ValidatorSet {
 	/// Get initial validator set
--- contracts/ValidatorVerifier.sol
+++ contracts/ValidatorVerifier.sol
@@ -1,4 +1,4 @@
-pragma solidity 0.5.9;
+pragma solidity ^0.5.11;
 
 import { ValidatorSet } from "./ValidatorSet.sol";
 
