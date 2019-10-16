1e263625fe96cd7c0c6a3c80264c157a7ea3be67
0xAshish <a4ashishrp@gmail.com>
2019-10-16 19:52:28 +0530

    remove all contracts

--- contracts/childChain/bor/StateReceiver.sol
+++
@@ -1,6 +0,0 @@
-pragma solidity ^0.5.11;
-
-// StateReceiver represents interface to receive state
-interface StateReceiver {
-  function onStateReceive(uint256 id, bytes calldata data) external;
-}
--- contracts/childChain/misc/IParentToken.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity ^0.5.11;
-//interface for parent contract of any child token
-
-
-interface IParentToken {
-  function beforeTransfer(address sender, address to, uint256 value) external returns(bool);
-}
