5771beb58db3f229fb1acf7076742eb9bc1df270
0xAshish <a4ashishrp@gmail.com>
2019-10-19 16:43:04 +0530

    pull new PR into submodule

--- contracts/Contracts.sol
+++ contracts/Contracts.sol
@@ -1,5 +1,5 @@
 pragma solidity ^0.5.11;
 
 import { MaticChildERC20 } from "../matic-contracts/contracts/child/MaticChildERC20.sol";
-import { BorValidatorSet } from "../matic-contracts/contracts/child/bor/BorValidatorSet.sol";
+import { BorValidatorSet } from "./BorValidatorSet.sol";
 import { StateReceiver } from "../matic-contracts/contracts/child/bor/StateReceiver.sol";
