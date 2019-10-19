5acaf67faa5bb8e55b26a8ad3549dbd33071f577
0xAshish <a4ashishrp@gmail.com>
2019-10-19 16:42:27 +0530

    add matic-contracts as submodule

--- contracts/Contracts.sol
+++ contracts/Contracts.sol
@@ -1,5 +1,5 @@
 pragma solidity ^0.5.11;
 
-import { MaticChildERC20 } from "matic-protocol/contracts/child/MaticChildERC20.sol";
-import { BorValidatorSet } from "matic-protocol/contracts/child/bor/BorValidatorSet.sol";
-import { StateReceiver } from "matic-protocol/contracts/child/bor/StateReceiver.sol";
+import { MaticChildERC20 } from "../matic-contracts/contracts/child/MaticChildERC20.sol";
+import { BorValidatorSet } from "../matic-contracts/contracts/child/bor/BorValidatorSet.sol";
+import { StateReceiver } from "../matic-contracts/contracts/child/bor/StateReceiver.sol";
