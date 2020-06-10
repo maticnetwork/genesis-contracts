71e7f2f5aec26be0a537944b68fab8ae5417b71c
Jaynti Kanani <jdkanani@gmail.com>
2020-06-10 18:34:34 +0530

    new: add generate sh file

---
+++ generate.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env sh
+
+# Usage: 
+# generate.sh 15001 heimdall-15001
+
+set -x #echo on
+
+if [ -z "$1" ]
+  then
+    echo "Bor chain id is required first argument"
+  exit 1
+fi
+
+if [ -z "$2" ]
+  then
+    echo "Heimdall chain id is required as second argument"
+  exit 1
+fi
+
+npm install
+npm run truffle:compile
+git submodule init
+git submodule update
+cd matic-contracts
+npm install
+node scripts/process-templates.js --bor-chain-id $1
+npm run truffle:compile
+cd ..
+npm run truffle:compile
+node generate-borvalidatorset.js --bor-chain-id $1 --heimdall-chain-id $2
+node generate-genesis.js --bor-chain-id $1 --heimdall-chain-id $2
