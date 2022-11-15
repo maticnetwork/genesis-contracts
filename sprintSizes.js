6cdba5d31488efb5339b271cba40ebb42cd48bf6
Arpit Temani <temaniarpit27@gmail.com>
2022-11-15 13:32:27 +0530

    add sprintsizes

---
+++ sprintSizes.js
@@ -0,0 +1,12 @@
+const blocks = [
+    {
+        number: "0",
+        sprintSize: "2"
+    },
+    {
+        number: "10",
+        sprintSize: "5"
+    }
+]
+
+exports = module.exports = sprintSizes
