2c461942c03aa1a7b74223f2c62690be844f7570
ethyla <36268899+ethyla@users.noreply.github.com>
2025-03-12 11:57:46 +0100

    feat: remove unused dep

--- generate-genesis.js
+++ generate-genesis.js
@@ -29,7 +29,7 @@ program.parse(process.argv)
 async function compileContract(key, contractFile, contractName, solcVersion) {
   return new Promise((resolve, reject) => {
     const ls = spawn(
-      `svm use ${solcVersion} && solc --bin-runtime openzeppelin-solidity/=node_modules/openzeppelin-solidity/ solidity-rlp/=node_modules/solidity-rlp/ /=/ ${contractFile}`,
+      `svm use ${solcVersion} && solc --bin-runtime openzeppelin-solidity/=node_modules/openzeppelin-solidity/ /=/ ${contractFile}`,
       { shell: true }
     )
 
